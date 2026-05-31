61bb7fbaa48ae92bb0d4d8d6ce6e8a4d4beff93e
DohuyaMa <siticenterzetech@gmail.com>
2026-05-31 05:58:18 +0200

    refactor: remove flake-parts and sub-flake, simplify to single flake.nix
    Replaced two-level flake structure (root + flake-global/flake.nix) and
    flake-parts dependency with a single flake.nix using flake-utils.lib.eachSystem.
    Removed flake-parts, nix-unit, and flake-global as flake inputs (15 inputs gone).
    All packages, devShells, apps, homeManagerModules and nixosModules are preserved.

--- flake.nix
+++ flake.nix
@@ -2,82 +2,33 @@
   description = "Real-time speech translation system";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
     flake-utils.url = "github:numtide/flake-utils";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-parts = {
-      url = "github:hercules-ci/flake-parts";
-      inputs.nixpkgs-lib.follows = "nixpkgs";
-    };
-    nix-unit = {
-      url = "github:nix-community/nix-unit";
-    };
-    flake-global.url = "path:./flake-global";
   };
-  outputs =
-    {
-      self,
-      nixpkgs,
-      flake-utils,
-      home-manager,
-      flake-parts,
-      nix-unit,
-      flake-global,
-      ...
-    }:
-    let
-      systems = [ "x86_64-linux" ];
-    in
-    flake-utils.lib.eachSystem systems (system: {
-      # Делегуємо всі пакети, devShells та apps на flake-global
-      packages = flake-global.packages.${system};
-      devShells = flake-global.devShells.${system};
-      apps = flake-global.apps.${system};
-    })
-    // {
-      # Home Manager модуль
-      homeManagerModules.rt-translator = flake-global.homeManagerModules.rt-translator;
 
-      # NixOS модулі (можна додати додаткові тут)
+  outputs = { self, nixpkgs, flake-utils, home-manager, ... }:
+    flake-utils.lib.eachSystem [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ] (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+        lib = nixpkgs.lib;
+        packages = (import ./flake-global/prod/packages.nix { inherit pkgs lib; }).packages;
+      in {
+        inherit packages;
+        devShells = (import ./flake-global/dev/devshell.nix { inherit pkgs; }).devShells;
+        apps.default = {
+          type = "app";
+          program = "${packages.default}/bin/translator-ui";
+        };
+      }
+    ) // {
+      homeManagerModules.rt-translator = import ./flake-global/home-manager-module.nix;
       nixosModules.virtual-sinks = import ./nixosModules/virtual-sinks.nix;
-
-      # Nix-unit тести
-      tests = {
-        "all python services are defined" = {
-          expr = builtins.attrNames (self.packages.x86_64-linux or { });
-          expected = [
-            "capture"
-            "whisper"
-            "translate"
-            "tts"
-            "playback"
-            "app"
-            "hybrid-whisper"
-            "ui"
-          ];
-        };
-
-        "systemd services exist" = {
-          expr = builtins.attrNames (self.nixosModules or { });
-          expected = [
-            "virtual-sinks"
-          ];
-        };
-
-        "flake inputs are properly defined" = {
-          expr = builtins.attrNames self.inputs;
-          expected = [
-            "nixpkgs"
-            "flake-utils"
-            "home-manager"
-            "flake-parts"
-            "nix-unit"
-            "flake-global"
-          ];
-        };
-      };
     };
 }
